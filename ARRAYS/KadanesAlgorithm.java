6ee2b8f2fca9a1daa4635cc5de0922b08e416bb2
Haseeb 4761 <mhaseebnawaz4761@gmail.com>
2024-08-29 23:02:21 +0500

    Add change

---
+++ ARRAYS/KadanesAlgorithm.java
@@ -0,0 +1,16 @@
+public class KadanesAlgorithm {
+    public static int kad_algo(int numbers[]){
+        int sum=0 , max = Integer.MIN_VALUE;
+        for (int i=0 ; i<numbers.length; i++){
+            sum += numbers[i];
+            if (sum<0)
+                sum = 0;
+            max = Math.max(max,sum);      
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+        int numbers[] = {1,-2,5,-3,4,6,-5};
+        System.out.println("The maximum sub array sum is " + kad_algo(numbers));
+    }
+}
