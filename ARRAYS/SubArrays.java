cb401c431e79acf74dba4a650a413a26c8030ed1
Haseeb 4761 <mhaseebnawaz4761@gmail.com>
2024-08-29 12:56:21 +0500

    add change

---
+++ ARRAYS/SubArrays.java
@@ -0,0 +1,18 @@
+public class SubArrays {
+    public static void sub_arrays(int numbers[]) {
+        for (int i=0; i<numbers.length; i++){
+            for (int j=i+1; j<=numbers.length; j++){
+                System.out.print("{ ");
+                for (int k=i; k<j ; k++){
+                    System.out.print(numbers[k] + " ");
+                }
+                System.out.print("},");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int numbers[] = {3,4,5,8,9};
+        sub_arrays (numbers);
+    }
+}
