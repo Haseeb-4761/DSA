8057a2be2a576edeebe2259f734fc3cb047a1974
Haseeb 4761 <mhaseebnawaz4761@gmail.com>
2024-08-29 14:19:49 +0500

    add change

--- ARRAYS/SubArrays.java
+++ ARRAYS/SubArrays.java
@@ -1,3 +1,4 @@
+// total subarrays = n(n+1)/2 where n is number of elements in a array
 public class SubArrays {
     public static void sub_arrays(int numbers[]) {
         for (int i=0; i<numbers.length; i++){
